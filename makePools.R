5081d796abaf5b4d933244adeb237d16c21be2f1
Jake Markman <jmmarkman@gmail.com>
2017-05-02 16:16:56 -0700

    Launch 2017

--- makePools.R
+++ makePools.R
@@ -1,37 +1,39 @@
 library(tidyr)
 roster <- read.csv("../currentRoster.csv")
 
-teamNo <- c(1:20, 1:20)
+teams_involved <- 25
+
+teamNo <- c(1:teams_involved, 1:teams_involved)
 teamNo <- teamNo[order(teamNo)]
 teamNo
 
-roster <- roster[order(roster$Team),]
+roster <- roster[order(roster$team_id),]
 roster$teamNo <- teamNo
 print(roster)
 
-set.seed(2016)
-pools <- rep(c("A","B","C","D","E"),4)
+set.seed(2017)
+pools <- rep(c("A","B","C","D","E"),5)
 pools <- sample(pools,length(pools))
 print(pools)
 
-idx <- 1:20
+idx <- 1:teams_involved
 pooler <- data.frame(idx, pools)
 print(pooler)
 
 roster <- merge(roster, pooler, by.x="teamNo", by.y="idx")
-roster$Name <- paste(roster$First.Name, roster$Last.Name)
+roster$Name <- paste(roster$first_name, roster$last_name)
 write.csv(roster, "../rosterPools.csv",row.names = FALSE)
 
-ppos <- rep(c("Player 1","Player 2"),20)
+ppos <- rep(c("Player 1","Player 2"),teams_involved)
 roster$ppos <- ppos
 allPool <- roster[,c("teamNo","Name","pools","ppos")]
 
 allPool <- spread(allPool, ppos, Name)
 names(allPool) <- c("Team Number", "Pool", "Player 1", "Player 2")
-allPool$Wins <- numeric(20)
-allPool$Losses <- numeric(20)
-allPool$Ties <- numeric(20)
-allPool$Points <- numeric(20)
-allPool$'Holes Won' <- numeric(20)
+allPool$Wins <- numeric(teams_involved)
+allPool$Losses <- numeric(teams_involved)
+allPool$Ties <- numeric(teams_involved)
+allPool$Points <- numeric(teams_involved)
+allPool$'Holes Won' <- numeric(teams_involved)
 
 write.csv(allPool,"poolPlay.csv",row.names=FALSE)
